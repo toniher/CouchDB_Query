68ff174be2ebcc8496c45f4ec0534c884efdc4f2
Toni Hermoso Pulido <toniher@cau.cat>
2018-01-03 11:37:16 +0100

    query params

--- includes/CouchDB_Lucene.php
+++ includes/CouchDB_Lucene.php
@@ -17,35 +17,39 @@ class CouchDB_Lucene {
 	
 						$db = $params["db"];
 
-						if ( array_key_exists( $db, $GLOBALS['wgCouchDB_Query']["params"] ) && array_key_exists( $db, $GLOBALS['wgCouchDB_Query']["queries"] ) ) {
+						if ( array_key_exists( $db, $GLOBALS['wgCouchDB_Query']["queries"] ) ) {
 
 							if ( array_key_exists( $index, $GLOBALS['wgCouchDB_Query']["queries"][$db] ) ) {
 
-								$urlquery = $GLOBALS['wgCouchDB_Query']["queries"][$db][ $index ];
-
-								$couchdb_params = $GLOBALS['wgCouchDB_Query']["params"][$db];
-
-								$auth = "";
-								if ( array_key_exists( "username", $couchdb_params ) && array_key_exists( "password", $couchdb_params ) ) {
-
-									$auth = $couchdb_params["username"].":".$couchdb_params["password"]."@";
-								}
-
 								// Let's force empty. If not explicitly put, retrieved from queries
+								$auth = "";
 								$protocol = "";
 								$portstr = "";
 								$host = "";
+							
+								$urlquery = $GLOBALS['wgCouchDB_Query']["queries"][$db][ $index ];
 
-								if ( array_key_exists( "protocol", $couchdb_params ) ) {
-									$protocol =  $couchdb_params["protocol"];
-								}
-
-								if ( array_key_exists( "port", $couchdb_params ) ) {
-									$portstr =  ":".$couchdb_params["port"];
-								}
-
-								if ( array_key_exists( "host", $couchdb_params ) ) {
-									$host =  $couchdb_params["host"];
+								if ( array_key_exists( $db, $GLOBALS['wgCouchDB_Query']["params"] ) ) {
+									
+									$couchdb_params = $GLOBALS['wgCouchDB_Query']["params"][$db];
+	
+									if ( array_key_exists( "username", $couchdb_params ) && array_key_exists( "password", $couchdb_params ) ) {
+	
+										$auth = $couchdb_params["username"].":".$couchdb_params["password"]."@";
+									}
+	
+									if ( array_key_exists( "protocol", $couchdb_params ) ) {
+										$protocol =  $couchdb_params["protocol"];
+									}
+	
+									if ( array_key_exists( "port", $couchdb_params ) ) {
+										$portstr =  ":".$couchdb_params["port"];
+									}
+	
+									if ( array_key_exists( "host", $couchdb_params ) ) {
+										$host =  $couchdb_params["host"];
+									}
+								
 								}
 
 								$lucene_params = array( "q", "limit", "skip" );
