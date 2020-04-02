f5a7e9bb3f2bf84d3c564be23e8ee6cb56b8b12f
Toni Hermoso Pulido <toniher@cau.cat>
2020-04-02 18:59:17 +0200

    prefixurl

--- CouchDB_Query_body.php
+++ CouchDB_Query_body.php
@@ -55,7 +55,7 @@ class CouchDB_Query {
 		$out->addModules( 'ext.CouchDB_Query' );
 
 		$returnhtml = "<div class='couchdb-query-table' data-text='".$attrs["text"]."' data-extra='".$attrs["extra"]."' data-total=0 data-skip=0 data-class='".$attrs["class"]."' data-db='".$attrs["db"]."' ".$startstr.$endstr;
-		$returnhtml.= " data-limit='".$attrs["limit"]."' data-header='".$attrs["header"]."' data-fields='".$attrs["fields"]."' data-query='".$attrs["query"]."' data-index='".$attrs["index"]."' data-prefix='".$attrs["prefix"]."' data-type='".$attrs["type"]."'></div>";
+		$returnhtml.= " data-limit='".$attrs["limit"]."' data-header='".$attrs["header"]."' data-fields='".$attrs["fields"]."' data-query='".$attrs["query"]."' data-index='".$attrs["index"]."' data-prefix='".$attrs["prefix"]."' data-prefixurl='".$attrs["prefixurl"]."' data-type='".$attrs["type"]."'></div>";
 
 		return array( $returnhtml, 'noparse' => true, 'isHTML' => true );
 
