a50a48effb7f436f1e73e28c8f6fca6ae6a8bf39
Toni Hermoso Pulido <toniher@cau.cat>
2020-04-20 11:37:34 +0200

    assing main.js

--- libs/main.js
+++ libs/main.js
@@ -8,7 +8,7 @@
 	$(document).ready(function(){
 		iterateTable();
 	});
-	
+
 
 	// Look for changes in the value
 	$( ".couchdb-query-table" ).on( "propertychange change click keyup input paste", "input", function(event){
@@ -18,12 +18,12 @@
 
 		timer = setTimeout(function() {
 			// If value has changed...
-	
+
 			if ( inputsave !== $(_this).val()) {
 				// Updated stored value
 				inputsave = $(_this).val();
 				var div = $( _this ).parents(".couchdb-query-table").first();
-	
+
 				$(div).data('text', inputsave );
 				if ( inputsave.length > 2 ) {
 					$(div).data('total', 0 );
@@ -33,7 +33,7 @@
 			}
 		}, 300, function() {});
 	});
-	
+
 	// Next, previous, detecting data-total and data-limit, etc.
 	$( ".couchdb-query-table" ).on( "click", ".bar > .next", function() {
 
@@ -45,7 +45,7 @@
 
 		iterateTable();
 	});
-	
+
 	$( ".couchdb-query-table" ).on( "click", ".prev", function() {
 
 		var div = $( this ).parents(".couchdb-query-table").first();
@@ -84,15 +84,14 @@
 			var db = $(div).data('db');
 			var text = $(div).data('text');
 			var extra = $(div).data('extra');
-			
+
 			var prefix = processPrefix( $(div).data('prefix') );
 			var prefixurl = processPrefix( $(div).data('prefixurl') );
 			var prefixcondurl = processPrefix( $(div).data('prefixcondurl') );
-			// TODO: To handle this
 
 			// Stricty necessary
 			if ( type !== "" && index !== "" && db !== "" ) {
-		
+
 				var params = {};
 
 				// Let's put bar
@@ -111,14 +110,14 @@
 				params["index"] = index;
 				params["db"] = db;
 				params["q"] = "";
-	
+
 				if ( limit !== "" ) {
 					params["limit"] = limit;
 				}
 				if ( skip !== "" ) {
 					params["skip"] = skip;
 				}
-				
+
 				if ( type.indexOf("lucene") > -1 ) {
 					params["q"] = subsTextQuery( query, text );
 
@@ -135,19 +134,19 @@
 						params["key"] = subsTextQuery( query, text );
 					}
 				}
-	
+
 				if ( $(div).data('start') ) {
 					params["start"] = $(div).data('start');
 				}
-	
+
 				if ( $(div).data('end') ) {
 					params["end"] = $(div).data('end');
 				}
-	
+
 				// GET QUERY here
 				params.action = type;
 				params.format = "json";
-				
+
 				var fieldse = fieldsp.split(",");
 				var fields = [];
 				for ( var s = 0; s < fieldse.length; s = s + 1 ) {
@@ -161,36 +160,36 @@
 						if ( data[type].status === "OK" ) {
 							if ( data[type].count ) {
 								$(div).data('total', data[type].count);
-	
+
 								var prev = ""; var next = ""; var count = "";
 								$(div).find("table").remove();
 								$(div).find(".bar > span").remove();
-	
+
 								if ( data[type].count > 0 ) {
 									count = "<span class='count'>" + data[type].count + "</span>";
 								}
-	
+
 								if ( ( ( data[type].count ) - parseInt( skip, 10 ) ) > parseInt( limit, 10 ) ) {
 									next = "<span class='next'>Next</span>";
 								}
 								if ( parseInt( skip, 10 ) > 0 ) {
 									prev = "<span class='prev'>Previous</span>";
 								}
-	
+
 								$(div).find(".bar").first().append(count+prev+next);
-	
+
 								if ( data[type].results.length > 0 ) {
-									var table = generateResultsTable( data[type].results, tableclass, header, fields, prefix, prefixurl );
+									var table = generateResultsTable( data[type].results, tableclass, header, fields, prefix, prefixurl, prefixcondurl );
 									$(div).append( table );
 									// generateSMWTable( $(div).children("table"), fields );
 									$(div).children("table").tablesorter(); //Let's make table sortable
-	
-								} 
+
+								}
 							} else {
 								$(div).find("table").remove();
 								$(div).find(".bar > span").remove();
 							}
-		
+
 						} else {
 							$(div).find("table").remove();
 							$(div).find(".bar > span").remove();
@@ -222,28 +221,28 @@
 
 		return query;
 	}
-	
+
 	function processPrefix( prefixstr ) {
-		
+
 		var prefix = {};
-		
+
 		if ( prefixstr ) {
-			
+
 			var prefixels = prefixstr.split(",");
-			
+
 			for ( var p=0; p < prefixels.length; p = p + 1 ) {
-			
+
 					var parts = prefixels[p].split(":");
-					
+
 					if ( parts.length === 2 ) {
 						prefix[parts[0]] = parts[1];
 					}
 			}
-			
+
 		}
 
 		return prefix;
-		
+
 	}
 
 	function processExtraFields( div, extra ) {
@@ -251,7 +250,7 @@
 		if ( extra ) {
 			if ( div ) {
 				var extras = extra.split(",");
-				
+
 				for ( var x = 0; x < extras.length; x = x + 1 ) {
 
 					var fieldDef = $( extras[x].trim() ).first(); // Let's assume only one
@@ -259,7 +258,7 @@
 						$(div).find(".extra").append( processExtraField( fieldDef ) );
 					}
 				}
-				
+
 			}
 		}
 	}
@@ -315,55 +314,149 @@
 					var subst = subsTextQuery( $(this).data('query'), val );
 					extraq = extraq + " " + subst;
 				}
-				
+
 			}
 		});
 
 		return extraq;
 	}
 
-	function generateResultsTable( results, tableclass, header, fields, prefix, prefixurl ) {
-	
+	function generateResultsTable( results, tableclass, header, fields, prefix, prefixurl, prefixcondurl ) {
+
 		var table = "<table class='" + tableclass + "'>";
 		table = table + "<thead><tr>";
-		
+
 		var headerstr = generateArrayTable( header, "th", " class=\"headerSort\" title=\"Sort ascending\"" );
 		table = table + headerstr;
-		
+
 		table = table + "</tr></thead>";
 		table = table + "<tbody>";
-		
+
 		for ( var r = 0; r < results.length; r = r + 1 ) {
 
-			var rowstr = generateRowTable( results[r], fields, "td", prefix, prefixurl );
+			var rowstr = generateRowTable( results[r], fields, "td", prefix, prefixurl, prefixcondurl );
 			table = table + "<tr>" + rowstr + "</tr>";
 		}
-		
+
 		table = table + "</tbody>";
 		table = table + "</table>";
-		
+
 		return table;
-	
+
 	}
-	
+
 	function generateArrayTable( arraystr, tag, extra ){
 		var str = "";
 		var array = arraystr.split(",");
 		extra = typeof extra !== 'undefined' ? extra : '';
-		
+
 		for ( var i = 0; i < array.length; i = i + 1 ) {
 				str = str + "<" + tag + extra + ">" + array[i] + "</" + tag + ">\n";
 		}
-	
+
 		return str;
 	}
-	
-	function generateRowTable( result, fields, tag, prefix, prefixurl ){
+
+	function getPrefixCondUrl( prefixcondurl ) {
+
+		let prefixHash;
+
+		let parts = prefixcondurl.split( "," )
+
+		for( part of parts ) {
+
+			let assigns = part.split( ":" );
+
+			if ( assigns.length === 2 ) {
+
+				let target = assigns[0];
+				if ( ! prefixHash.hasOwnProperty( target ) ) {
+					prefixHash[ target ] = {};
+				}
+
+				let values = assigns[1].split( "=" );
+
+				if ( values.length === 2 ) {
+
+					let prefix = values[1];
+
+					let conds = values[0].split( "@" );
+
+					if ( conds.length === 2 ) {
+
+						let cond = conds[0];
+						let val = conds[1];
+
+						if ( ! prefixHash[target].hasOwnProperty( cond ) ) {
+							 prefixHash[target][cond] = {};
+						}
+
+						if ( ! prefixHash[target][cond].hasOwnProperty( val ) ) {
+							 prefixHash[target][cond][val] = {};
+						}
+
+						prefixHash[target][cond][val] = prefix;
+
+					}
+
+				}
+
+			}
+
+		}
+
+
+		return prefixHash;
+
+	}
+
+	function assignUrlPrefix( assign, fields ){
+		let prefix = "";
+
+		let case;
+		for ( let c in assign ) {
+
+			if ( assign.hasOwnProperty( c ) ) {
+
+				case = c;
+			}
+		}
+
+		let val;
+		for ( let v in assign[case] ) {
+
+			if ( assign[case].hasOwnProperty( v ) ) {
+
+				val = v;
+			}
+
+		}
+
+		for( let f in fields ) {
+
+			if ( f == case ) {
+
+				let occur = fields[f];
+
+				if ( occur == val ) {
+					prefix = assign[case][val];
+				}
+
+			}
+		}
+
+		return prefix;
+
+	}
+
+	function generateRowTable( result, fields, tag, prefix, prefixurl, prefixcondurl ){
 		var str = "";
-		
+
+		var prefixHash = getPrefixCondUrl( prefixcondurl );
+
 		for ( var i = 0; i < fields.length; i = i + 1 ) {
 			var field = fields[i];
-		
+
 			var prop = "";
 			var pagename = null;
 			var fieldTxt = "";
@@ -409,37 +502,51 @@
 				}
 				if ( result.hasOwnProperty("fields") && result["fields"].hasOwnProperty(field) ) {
 					fieldTxt = result["fields"][field];
-					
+
 					// Detect here if in prefix. If so, append
 					if ( prefix.hasOwnProperty( field ) ) {
 						fieldTxt = prefix[field]+":"+fieldTxt;
 					}
-										
+
 					if ( pagelink ) {
 						fieldTxt = fieldTxt.replace("#", "/");
 
 						if ( prefixurl.hasOwnProperty( field ) ) {
 							url = prefixurl[field]+fieldTxt;
 						}  else {
-							url = mw.config.get( "wgArticlePath" ).replace('$1', fieldTxt );
+
+							if ( prefixHash.hasOwnProperty( field ) ) {
+								url = assignUrlPrefix( prefixHash[field], result["fields"] )+fieldTxt;
+							} else {
+
+								url = mw.config.get( "wgArticlePath" ).replace('$1', fieldTxt );
+							}
 						}
+
+
+
 						fieldTxt = "<a href='" + url +"'>" + fieldTxt + "</a>";
 					}
 					if ( cleanpagelink ) {
-						
+
 						fieldTxt = fieldTxt.replace("#", "/");
 
 						if ( prefixurl.hasOwnProperty( field ) ) {
 							url = prefixurl[field]+fieldTxt;
 						} else {
-							url = mw.config.get( "wgArticlePath" ).replace('$1', fieldTxt );
+							if ( prefixHash.hasOwnProperty( field ) ) {
+								url = assignUrlPrefix( prefixHash[field], result["fields"] )+fieldTxt;
+							} else {
+
+								url = mw.config.get( "wgArticlePath" ).replace('$1', fieldTxt );
+							}
 						}
-						
+
 						fieldTxt = fieldTxt.replace(/\@\S+/, "");
 						fieldTxt = fieldTxt.replace(/^\S+\:/, "");
 
 						fieldTxt = "<a href='" + url +"'>" + fieldTxt + "</a>";
-					}		
+					}
 				}
 			}
 			prop = " data-prop='"+field+"' ";
@@ -447,22 +554,22 @@
 		}
 		return str;
 	}
-	
-	
+
+
 	function generateSMWTable( tables, fields ){
-		
+
 		var fieldsSMW = [];
-		
+
 		for ( var i = 0; i < fields.length; i = i + 1 ) {
 			if ( ! fields[i].startsWith("*") ) {
 				fieldsSMW.push( fields[i] );
 			}
 		}
-		
+
 		$(tables).each( function( i ) {
-		
+
 			$(this).find("tbody > tr").each( function( r ) {
-		
+
 				//console.log( this );
 				var row = this;
 				var pagename = $(row).children("td").filter("[data-prop='*']").first().text();
@@ -471,13 +578,13 @@
 				}
 				// Generate ask query from this
 				if ( pagename ) {
-		
+
 					var params = {};
 					params.action = "askargs";
 					params.conditions = pagename;
 					params.printouts = fieldsSMW.join("|");
 					params.format = "json"; // Let's put JSON
-					
+
 					var posting = $.get( mw.config.get( "wgScriptPath" ) + "/api.php", params );
 					posting.done(function( out ) {
 						if ( out && out.hasOwnProperty("query") ) {
@@ -510,7 +617,7 @@
 					});
 				}
 			});
-		
+
 		});
 	}
 
@@ -531,5 +638,5 @@
     couchjsfunc.test = function( text ) {
 		return "test";
     };
- 
+
 }( window.couchjsfunc = window.couchjsfunc || {}, jQuery ));
