1fd14aab6070d3b1586ff23cc265325163e7f5f5
Toni Hermoso Pulido <toniher@cau.cat>
2020-06-25 16:12:39 +0200

    replace char

--- includes/CouchDB_Document.php
+++ includes/CouchDB_Document.php
@@ -14,7 +14,7 @@ class CouchDB_Document {
 				if ( array_key_exists( "db", $params ) ) {
 
 					if ( ! empty( $params["db"] ) ) {
-	
+
 						$db = $params["db"];
 
 						// Let's force empty. If not explicitly put, retrieved from queries
@@ -22,9 +22,9 @@ class CouchDB_Document {
 						$protocol = "";
 						$portstr = "";
 						$host = "";
-					
+
 						if ( array_key_exists( $db, $GLOBALS['wgCouchDB_Query']["document"] ) ) {
-							
+
 							$couchdb_params = $GLOBALS['wgCouchDB_Query']["document"][$db];
 
 							if ( array_key_exists( "username", $couchdb_params ) && array_key_exists( "password", $couchdb_params ) ) {
@@ -43,17 +43,18 @@ class CouchDB_Document {
 							if ( array_key_exists( "host", $couchdb_params ) ) {
 								$host =  $couchdb_params["host"];
 							}
-						
+
 						}
 
 						$url = $auth.$host.$portstr."/".$db."/".$key;
-		
+
 						if ( ! empty( $protocol ) ) {
-							
+
 							$url = $protocol."://". $url;
 						}
-						
+
 						$url = str_replace( " ", "%20", $url );
+						$url = str_replace( "#", "%23", $url );
 
 						$json = file_get_contents( $url );
 
